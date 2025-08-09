fedeb3a555e67d1192a2e6d39ff715a85f9ee4ef
AjayJoshi119418 <ajay.joshi119418@marwadiuniversity.ac.in>
2025-08-09 10:34:12 +0530

    Update FetchApi.js

--- src/FetchApi.js
+++ src/FetchApi.js
@@ -6,15 +6,15 @@ function FetchApi() {
     const [location, setLocation] = useState("");
 
     function print() {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${pi}&appid=${'69a33d4e32ce897f1db90f15898387f6'}&units=metric`).
-            then(response => response.json()).
-            then(data => spd(data)).
-            catch(error => console.log(error));
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${pi}&appid=${'69a33d4e32ce897f1db90f15898387f6'}&units=metric`)
+            .then(response => response.json())
+            .then(data => spd(data))
+            .catch(error => console.log(error));
            
-        fetch(`https://api.unsplash.com/photos/random?client_id=${'jk8Yguj4bqDXkNTBxUGNnbxyNCn_NH0SvvLtfz-ARws'}&query=${pi}`).
-        then(response => response.json()).
-        then(data => setLocation(data?.urls.small_s3)).
-        catch(error => console.log(error));
+        fetch(`https://api.unsplash.com/photos/random?client_id=${'jk8Yguj4bqDXkNTBxUGNnbxyNCn_NH0SvvLtfz-ARws'}&query=${pi}`)
+            .then(response => response.json())
+            .then(data => setLocation(data?.urls.small_s3))
+            .catch(error => console.log(error));
     }
     useEffect(()=>{
     document.body.style.backgroundImage = `url(${location})`
